cfdb6ac0e592a32a60695faedfacbcd64d529a32
Isaacdevs <imthimun@student.wethinkcode.co.za>
2022-06-28 18:46:01 +0200

    latest webapp version

--- base_app.py
+++ base_app.py
@@ -24,6 +24,7 @@
 # Streamlit dependencies
 import streamlit as st
 import joblib,os
+from PIL import Image
 
 # Data dependencies
 import pandas as pd
@@ -39,19 +40,82 @@ raw = pd.read_csv("resources/train.csv")
 def main():
 	"""Tweet Classifier App with Streamlit """
 
+
+	st.set_page_config(page_title="Data Innov", page_icon=":earth_africa:")
+
 	# Creates a main title and subheader on your page -
 	# these are static across all pages
-	st.title("Tweet Classifer")
-	st.subheader("Climate change tweet classification")
+	title_logo, title = st.columns((1,5))
+
+	with title_logo:
+		st.image("resources/images/logo1 (1).png")
+
+	with title:
+		st.title("""DATA INNOV CLASSIFIERS""")
 
 	# Creating sidebar with selection box -
 	# you can create multiple pages this way
-	options = ["Prediction", "Information"]
-	selection = st.sidebar.selectbox("Choose Option", options)
+	options = ["About Us", "Prediction", "Information"]
+	selection = st.sidebar.radio("Navigation", options)
+
+
+
+	if selection == "About Us":
+		st.write("---")
+		st.header("About the Team")
+		st.write("##")
+
+		team_img, team_info = st.columns((1,3))
+
+		with team_img:
+			st.image('resources/images/team.jpeg')
+		with team_info:
+			st.write(
+				"""
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+				nost
+				
+				"""
+			)	
+
+
+		st.header("About the Plaform")
+		st.write("##")
+
+		project_info, project_img = st.columns((2,1))
+
+		with project_info:
+			st.write(
+				"""
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor
+				incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
+				nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				 eu fugiat nulla pariatur. Excepteur sint occaeca
+
+
+
+				nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				 eu fugiat nulla pariatur. Excepteur sint occaeca
+
+				nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. 
+				Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore
+				 eu fugiat nulla pariatur. Excepteur sint occaeca  
+				
+				"""
+			)		
+
+		with project_img:
+			st.image('resources/images/climate-change.jpg')
+			st.image('resources/images/climate-change2.jpeg')
+
+
+
 
 	# Building out the "Information" page
 	if selection == "Information":
-		st.info("General Information")
 		# You can read a markdown file from supporting resources folder
 		st.markdown("Some information here")
 
@@ -61,7 +125,7 @@ def main():
 
 	# Building out the predication page
 	if selection == "Prediction":
-		st.info("Prediction with ML Models")
+		st.subheader("Climate change tweet classification")
 		# Creating a text box for user input
 		tweet_text = st.text_area("Enter Text","Type Here")
 
@@ -76,6 +140,7 @@ def main():
 			# When model has successfully run, will print prediction
 			# You can use a dictionary or similar structure to make this output
 			# more human interpretable.
+			st.snow()
 			st.success("Text Categorized as: {}".format(prediction))
 
 # Required to let Streamlit instantiate our web app.  
